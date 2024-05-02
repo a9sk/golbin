72b70cded5f51a745157578d5458e6927d2f52b6
a9sk <920a9sk42f76c765@proton.me>
2024-05-02 18:33:03 +0200

    seclist installer function

--- golbin.sh
+++ golbin.sh
@@ -11,8 +11,8 @@ nmap_installer(){
             sudo apt-get install nmap
 		else
 			case $ok in
-				y|Y|yes|Yes|YES)	sudo apt-get update; sudo apt-get install nmap; echo "Nmap was installed with success." ;;
-				n|N|no|No|NO)		echo "[*] Moving on then" ; echo "[!] Exiting" ; exit ;;
+				y|Y|yes|Yes|YES)	sudo apt-get update; sudo apt-get install nmap;;
+				n|N|no|No|NO)		echo "[*] Nmap is needed" ; echo "[!] Exiting" ; exit ;;
 			esac
 		fi
     else
@@ -24,7 +24,29 @@ nmap_installer(){
 		else
 			case $up in
 				y|Y|yes|Yes|YES)	sudo apt-get update; sudo apt-get upgrade nmap;;
-				n|N|no|No|NO)		echo "[*] Moving on then" ; echo "[!] Exiting" ; exit ;;
+				n|N|no|No|NO)		echo "[!] Might have some problems with old nmap versions" ; sleep 1; echo "[*] Moving on then" ; sleep 1;;
+			esac
+		fi
+    fi
+}
+
+seclist_installer(){
+    if [ -d "SecLists-master" ]
+    then
+        echo '[!] SecList already installed'
+        sleep 1
+        echo '[*] Continue...'
+    else
+        read -p "$(echo -e '[?] SecList not installed or in a different path, do you want to install it here? (Will take a few minutes, if already installed move it here)'$i' (y/n) ')" ok
+		if [[ $ok == "" ]]
+		then
+			wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip
+            unzip SecList.zip
+            rm -f SecList.zip
+		else
+			case $ok in
+				y|Y|yes|Yes|YES)    wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip; echo '[*] Install unzip to continue'; sudo apt install unzip; echo '[*] Unzipping the whole SecList.zip (might take a few minutes)'; unzip SecList.zip; rm -f SecList.zip ;;
+				n|N|no|No|NO)		echo "[*] SecList is needed" ; sleep 1; echo "[!] Exiting" ; sleep 1; exit ;;
 			esac
 		fi
     fi
@@ -32,7 +54,7 @@ nmap_installer(){
 
 main() {
     clear
-    
+    sleep 1
     echo "    **********************************************"
     echo "    ******             GolBin               ******"
     echo "    **********************************************"
@@ -41,8 +63,12 @@ main() {
     echo "    Golbin v1.0.0 @a9sk"
     echo "      "
     echo "      "
-
+    sleep 1
     nmap_installer
+    sleep 1
+    clear
+    sleep 1
+    seclist_installer
 
 }
 
